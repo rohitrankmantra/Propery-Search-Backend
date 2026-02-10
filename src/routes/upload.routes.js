7ef037577cb9a678d16795189938992c3c8f8bbe
developmentrankmantra-sudo <developmentrankmantra@gmail.com>
2026-02-10 16:50:49 +0530

    rpute udpated succeesfully

--- src/routes/upload.routes.js
+++ src/routes/upload.routes.js
@@ -1,11 +1,10 @@
 import express from "express";
 import upload from "../middleware/multer.middleware.js";
-import { adminAuth } from "../middleware/auth.middleware.js";
 import { uploadImage } from "../controllers/upload.controller.js";
 
 const router = express.Router();
 
 // Single image upload
-router.post("/", adminAuth, upload.single("image"), uploadImage);
+router.post("/", upload.single("image"), uploadImage);
 
 export default router;
